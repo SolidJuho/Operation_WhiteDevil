34b87b080b1b84057283435a03c2a59c53355826
Juho Vatanen <juho.vatanen@gmail.com>
2022-05-27 16:46:08 +0300

    Task manager done!

--- Scripts/Game/Coop/SCR_CoopTaskManager.c
+++ Scripts/Game/Coop/SCR_CoopTaskManager.c
@@ -14,9 +14,14 @@ class SCR_CoopTaskManager : SCR_BaseTaskManager
 
 	//! Runtime instances for tasks created from initial task names
 	protected ref array<SCR_BaseTask> m_aInitialTasks;
-
+	
+	protected ref array<SCR_BaseTask> m_AdditionalTasks;
+	protected ref array<SCR_BaseTask> m_FailedTasks;
+	
 	protected RplComponent m_pRplComponent;
 	
+	[Attribute()]
+	protected ref array<ref SolidObjectives> AdditionalObjectives;
 	
 	//------------------------------------------------------------------------------------------------
 	protected override void EOnInit(IEntity owner)
@@ -64,21 +69,95 @@ class SCR_CoopTaskManager : SCR_BaseTaskManager
 
 			task.SetTargetFaction(targetFaction);
 		}
-		
-		
+
 	}
 
-	//------------------------------------------------------------------------------------------------
-	protected override void OnPlayerRegistered(int registeredPlayerID)
+	
+	void UnlockObjective(string unlockKey)
 	{
-		super.OnPlayerRegistered(registeredPlayerID);
-
-		SCR_BaseTaskExecutor taskExecutor = SCR_BaseTaskExecutor.GetTaskExecutorByID(registeredPlayerID);
-		if (!taskExecutor)
-		{
-			Print("No execution can be executed because executing execution execute exec.");
+	
+		// Do not initialize these tasks out of runtime
+		if (!GetGame().InPlayMode())
 			return;
+
+		m_AdditionalTasks = {};
+		BaseWorld world = this.GetWorld();
+
+		SolidObjectives objData;
+		foreach (SolidObjectives obj : AdditionalObjectives)
+		{
+			if(obj.m_UnlockKey == unlockKey)
+			{
+				objData = obj;
+				break;
+			}
 		}
 		
+		foreach (string taskName : objData.m_TaskNames)
+		{
+			IEntity entity = world.FindEntityByName(taskName);
+			SCR_BaseTask task = SCR_BaseTask.Cast(entity);
+			if (!task)
+				continue;
+			
+			m_AdditionalTasks.Insert(task);
+		}
+		
+		m_pRplComponent = RplComponent.Cast(this.FindComponent(RplComponent));
+		if (!m_pRplComponent)
+		{
+			Print("SCR_CoopTaskManager is missing m_pRplComponent!", LogLevel.ERROR);
+			return;
+		}
+
+		// Authority only
+		if (!m_pRplComponent.IsMaster())
+			return;
+
+		Faction targetFaction = GetGame().GetFactionManager().GetFactionByKey(m_sAssignedFaction);
+		if (!targetFaction)
+		{
+			Print("SCR_CoopTaskManager is missing Faction to assign tasks to!", LogLevel.ERROR);
+			return;
+		}
+
+		foreach (SCR_BaseTask task : m_AdditionalTasks)
+		{
+			if (!task)
+				continue;
+
+			task.SetTargetFaction(targetFaction);
+		}
+		
+		foreach (string taskName : objData.m_FailedTaskNames)
+		{
+			IEntity entity = world.FindEntityByName(taskName);
+			SCR_BaseTask task = SCR_BaseTask.Cast(entity);
+			if (!task)
+				continue;
+			
+			m_FailedTasks.Insert(task);
+		}
+		foreach (SCR_BaseTask fTask : m_FailedTasks)
+		{
+			GetTaskManager().FailTask(fTask);
+		}
 	}
 }
+
+[BaseContainerProps(), BaseContainerCustomTitleField("m_UnlockKey")]
+class SolidObjectives 
+{
+	[Attribute("", UIWidgets.EditBox, "Unlock keyword.", category: "TaskManager: COOP")]
+	string m_UnlockKey;
+	
+	[Attribute("", UIWidgets.Auto, "Entity names of tasks to be unlocked.", category: "TaskManager: COOP")]
+	ref array<string> m_TaskNames;
+	
+	[Attribute("", UIWidgets.Auto, "Entity names of tasks that fails when this activates.", category: "TaskManager: COOP")]
+	ref array<string> m_FailedTaskNames;
+}
+
+
+
+
